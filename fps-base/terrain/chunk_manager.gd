cda5a7037a751e8e26bfdaab396422d118b0a4b6
Esrael509 <helblem@outlook.es>
2026-09-17 17:26:26 -0600

    otro
    otro

--- fps-base/terrain/chunk_manager.gd
+++ fps-base/terrain/chunk_manager.gd
@@ -14,8 +14,8 @@ extends Node3D
 @export var render_distance: int = 1     # radio (en chunks) con malla de máximo detalle
 @export var far_distance: int = 5       # radio con LOD reducido (sin colisión)
 @export var collision_distance: int = 2  # radio con collider físico activo
-@export var world_seed: int = 987651
-@export var max_far_chunks_per_frame: int = 8  # throttle SOLO para lo no-crítico
+@export var world_seed: int = 12345
+@export var max_far_chunks_per_frame: int = 16  # throttle SOLO para lo no-crítico
 
 @export_group("Rango visual (opcional, recomendado)")
 @export var camera_path: NodePath            # tu Camera3D -> ajusta el far clip automáticamente
